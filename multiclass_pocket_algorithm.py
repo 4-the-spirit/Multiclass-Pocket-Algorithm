cc49ced447ad3b864d90e50f94f34fb8226a746b
Tamir Globus <168824894+4-the-spirit@users.noreply.github.com>
2024-12-03 12:45:17 +0200

    Update multiclass_pocket_algorithm.py

--- multiclass_pocket_algorithm.py
+++ multiclass_pocket_algorithm.py
@@ -2,6 +2,15 @@ from pocket_algorithm import PocketAlgorithm
 
 
 class MultiClassPocketAlgorithm:
+  """
+    Implementation of a Multi-Class Classifier using the Pocket Algorithm.
+
+    This algorithm extends the binary classification Pocket Algorithm to handle 
+    multi-class classification problems by training a separate classifier for each class
+    in a one-vs-all manner. The best weights and bias for each classifier are determined
+    using the Pocket Algorithm.
+  """
+  
   def __init__(self, training_data, regular_training_labels):
     self._training_data = training_data
     self._regular_training_labels = regular_training_labels
@@ -9,12 +18,11 @@ class MultiClassPocketAlgorithm:
     self._classification_classes = None
     self._weights_vec = None
     self._b = None
-    self._error_rate = None
 
   @property
   def classification_classes(self):
     if self._classification_classes is None:
-      self._classification_classes = list(set(self.regular_training_labels))
+      self._classification_classes = list(set(tuple(x.astype(int)) for x in self.regular_training_labels))
     return self._classification_classes
 
   @property
@@ -38,16 +46,6 @@ class MultiClassPocketAlgorithm:
     self._b = value
 
   @property
-  def error_rate(self):
-    if self._error_rate is None:  
-      misclassified = 0
-      for i in range(len(self.training_data)):
-        if self.classify(self.training_data[i]) != self.regular_training_labels[i]:
-          misclassified += 1
-      self._error_rate = misclassified / len(self.training_data)
-    return self._error_rate
-
-  @property
   def training_data(self):
     return self._training_data
 
@@ -55,8 +53,20 @@ class MultiClassPocketAlgorithm:
   def regular_training_labels(self):
     return self._regular_training_labels
 
+  @property
+  def misclassified(self):
+    misclassified = 0
+    for i in range(len(self.training_data)):
+      if self.classify(self.training_data[i]) != tuple(self.regular_training_labels[i].astype(int).tolist()):
+        misclassified += 1
+    return misclassified
+
+  @property
+  def error_rate(self):
+    return self.misclassified / len(self.regular_training_labels)
+
   def apply_mapping(self, mapping):
-    return [mapping[label] for label in self.regular_training_labels]
+    return [mapping[tuple(label.tolist())] for label in self.regular_training_labels]
 
   def create_mapping(self, classification_class):
     mapping = dict()
@@ -67,7 +77,7 @@ class MultiClassPocketAlgorithm:
         mapping[label] = PerceptronAlgorithm.NEGATIVE_CLASS_VALUE
     return mapping
 
-  def run(self, iterations):
+  def run(self, updates, max_epochs=1):
     for index in range(len(self.classification_classes)):
       initial_weights_vec = self.weights_vec[index]
       initial_b = self.b[index]
@@ -75,11 +85,10 @@ class MultiClassPocketAlgorithm:
       mapping = self.create_mapping(positive_label)
       training_labels = self.apply_mapping(mapping)
 
-      perceptron = PocketAlgorithm(self.training_data, training_labels)
-      perceptron = perceptron.from_parameters(initial_weights_vec, initial_b)
-      perceptron.run(iterations)
-      self.weights_vec[index] = perceptron.weights_vec
-      self.b[index] = perceptron.b
+      pocket = PocketAlgorithm(self.training_data, training_labels).from_parameters(initial_weights_vec, initial_b)
+      pocket.run(updates, max_epochs)
+      self.weights_vec[index] = pocket.weights_vec
+      self.b[index] = pocket.b
     return None
 
   def argmax(self, data):
